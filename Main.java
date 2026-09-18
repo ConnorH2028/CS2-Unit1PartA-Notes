3b763e2de74bc9e2ef5a46dba72688f72691b00a
ConnorH2028 <connor.hamilton2028@gbwl.org>
2026-09-18 16:40:34 +0000

    more notes

--- Main.java
+++ Main.java
@@ -1,5 +1,21 @@
 /*
-This is my comment space! Wohoo.
+This is my comment space! Wohoo!
+Algorithm --> step by step process to accomp a task
+Pseudocode --> simplified code to outline programs/algorithms
+ex. addfunc()
+num1
+num2
+1+2 prints
+sequencing: order of steps
+Java:
+- ment or a vtural machien
+- diffrent plug in
+- It a OOP programing language
+- stronger program
+Javascript
+- ment for only browser usage
+- diffrent plug in
+- Its an OOP Spricpting language
  */
 
 

ff45ce68d6e3fd777ae51a5a57dbc2dc537499fd
ConnorH2028 <connor.hamilton2028@gbwl.org>
2026-09-22 14:03:12 +0000

    Even more notes

--- Main.java
+++ Main.java
@@ -1,4 +1,5 @@
 /*
+9/18
 This is my comment space! Wohoo!
 Algorithm --> step by step process to accomp a task
 Pseudocode --> simplified code to outline programs/algorithms
@@ -16,7 +17,14 @@ Javascript
 - ment for only browser usage
 - diffrent plug in
 - Its an OOP Spricpting language
- */
+9/22
+- Object Oriented Programming - Programming built on classes and objects    
+- Class - Blueprint of an object that has no memory 
+- Object - Actual implementation that is stored in memory 
+- Method- Reusable chunk of code that accomplishes an action 
+- Main() - entry point to our code. We code in an IDE with a compiler
+- Compiler- Translate our java to binary 
+*/
 
 
 

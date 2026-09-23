915afab95dc63dd84dc045944b772ea76ddebc14
ConnorH2028 <connor.hamilton2028@gbwl.org>
2026-09-23 18:36:19 +0000

    More Notes...

--- Main.java
+++ Main.java
@@ -18,12 +18,33 @@ Javascript
 - diffrent plug in
 - Its an OOP Spricpting language
 9/22
-- Object Oriented Programming - Programming built on classes and objects    
+- Object Oriented Programming --> Programming built on classes and objects    
 - Class --> Blueprint of an object that has no memory 
 - Object --> Actual implementation that is stored in memory 
 - Method --> Reusable chunk of code that accomplishes an action 
 - Main() --> entry point to our code. We code in an IDE with a compiler
 - Compiler--> Translate our java to binary 
+9/23
+- Variable --> A labeled memory location in the computer that can store a value that can change or vary while a program is running.
+- There are two types of variables in Java:
+ - Primitive Type - storing simple information.data (ex. int x = 5;)
+ - Object (Reference) Type - storing complex data/objects (ex. Creature cat = new Creature)
+
+ -- Primitive Variable Types To Know:
+ 1. int - stores intergers/positive or negative whole numbers
+ 2. double- store decimal numbers (ex. double x = 5.0;) (ex. double y = 4.25;)
+ 3. boolean - stores logic (only two options are True or False)
+
+Object Variable Type to Know:
+1. String - stores text (ex. "5.0", "Hello, world!")
+
+Setting Up Variables In Code:
+Declaring + Assigning go togeather
+1. Decalring Variable --> int x; String name;
+2. Assign Variable --> x = 5; name = "Connor"
+
+Or do it in one step!
+3. Initialize Variable --> int x=5; String name = "Connor"
 */
 
 
